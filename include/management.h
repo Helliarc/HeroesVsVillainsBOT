1f9edfb678dda48587965dcfb2b994226466fbc5
Helliarc <43101897+Helliarc@users.noreply.github.com>
2022-03-05 03:48:21 +0000

    Update management.h

--- include/management.h
+++ include/management.h
@@ -1,12 +1,11 @@
 #pragma once
-
-/*All functions here forward declare the specific management function definitions to manage the RVB BOT.*/
+#include <dpp/dpp.h>
 
 namespace RVB_m {
 
-	void updateGuildRoles(dpp::cluster& bot, dpp::snowflake GID, std::unordered_map<dpp::snowflake, dpp::role_map>& connectedGuildRoles);
-
-	//void buildChannels(dpp::cluster& bot, dpp::snowflake GID);
-
-	bool buildRole(dpp::cluster& bot, dpp::snowflake GID, std::string roleName);
+	void roleCheck(dpp::cluster& bot, dpp::snowflake& GID);
+	void categoryCreate(dpp::cluster& bot, dpp::snowflake GID);
+	void openInvite(dpp::cluster& bot, dpp::snowflake GID);
+	void roleClick(dpp::cluster& bot, dpp::button_click_t event);
+	
 }
