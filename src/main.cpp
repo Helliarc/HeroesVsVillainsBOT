a9286389b3c51659b59813af1bc3f4cc2af9abfb
Helliarc <43101897+Helliarc@users.noreply.github.com>
2022-03-05 03:49:03 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,227 +1,94 @@
-
-#include <dpp/dpp.h>
-#include <dpp/nlohmann/json.hpp>
-
-#include "../../envars.h"
-#include "utility.h"
 #include "initialization.h"
 #include "management.h"
 
+#include <dpp/dpp.h>
 #include <iostream>
-#include <fstream>
-#include <unordered_map>
+
+#include "../../envars.h"
 
 int main()
 {
-    std::unordered_map<dpp::snowflake, dpp::role_map> connectedGuildRoles{};
+    dpp::cluster bot(ROLESTOKEN);
 
-    try {
-
-        dpp::cluster bot(ROLESTOKEN, dpp::intents::i_all_intents);
-
-        bot.on_ready([&bot](const dpp::ready_t& event) {
-            bot.log(dpp::ll_info, "Logged in as " + bot.me.username);
-
-            RVB_i::startupVerification(bot);  //When bot resets, this is the first thing the RVB Bot does
-
-        });
-
-        bot.on_button_click([&bot, &connectedGuildRoles](const dpp::button_click_t& event) {
+    bot.on_interaction_create([&bot](const dpp::interaction_create_t& event) {
+        if (event.command.type == dpp::it_application_command) {
+            auto cmd_data = std::get<dpp::command_interaction>(event.command.data);
             auto GID = event.command.guild_id;
-            auto commandUser = event.command.member;
 
-            if (event.custom_id == "blue") {
-                event.reply(dpp::ir_channel_message_with_source, dpp::message("Attempting to join the ***Blue*** team...").set_flags(dpp::m_ephemeral));
-                dpp::snowflake roleIDRed{};
-                dpp::snowflake roleIDBlue{};
-                dpp::snowflake roleIDCivilian{};
-                RVB_u::getGuildRoleIDs(roleIDRed, roleIDBlue, roleIDCivilian, connectedGuildRoles, GID);                
-                bool isRed = false;
-                bool isBlue = false;
+            if (cmd_data.name == "init") {
+                RVB_i::createCommands(bot, GID);                            //create the slash commands necessary for RVB BOT monitoring.
+                RVB_m::roleCheck(bot, GID);                                 //initializes the roles check/creation
 
-                for (auto r : commandUser.roles) {
-                    if (r == roleIDRed) {
-                        isRed = true;
-                    }
-                    if (r == roleIDBlue) {
-                        isBlue = true;
-                    }
-                }
+                event.reply("Initializing the RedVsBlue BOT...");
+            }
+            if (cmd_data.name == "createchannels") {
+                RVB_m::categoryCreate(bot, GID);                            //initializes the channels check/creation
 
-                if (isRed == false && isBlue == false) {
-                    bot.guild_member_add_role(GID, commandUser.user_id, roleIDBlue,
-                        [commandUser, roleIDBlue, event](const dpp::confirmation_callback_t& event1) {
-                            if (event1.is_error()) {
-                                std::cout << "Error changing guild member role." << event1.http_info.body << std::endl;
-                            }
-                            else {
-                                std::cout << "Success assigning member: " << commandUser.user_id << " the role: " << roleIDBlue << std::endl;
-                                event.edit_response(dpp::message("Success joining the ***Blue*** team...").set_flags(dpp::m_ephemeral));
-                            }
-                        });
-                }
-
-                if (isRed == true) {
-                    event.edit_response(dpp::message("You are on team ***Red***! Get out of here you SPY!!! **You cannot change roles after choosing one**").set_flags(dpp::m_ephemeral));
-                }
-
-                if (isBlue == true) {
-                    event.edit_response(dpp::message("You are already on the ***Blue*** team.").set_flags(dpp::m_ephemeral));
-                }
+                event.reply("Creating the RedVsBlue channels...");
             }
 
-            if (event.custom_id == "red") {
-                event.reply(dpp::ir_channel_message_with_source, dpp::message("Attempting to join the ***Red*** team...").set_flags(dpp::m_ephemeral));
-                dpp::snowflake roleIDRed{};
-                dpp::snowflake roleIDBlue{};
-                dpp::snowflake roleIDCivilian{};
-                RVB_u::getGuildRoleIDs(roleIDRed, roleIDBlue, roleIDCivilian, connectedGuildRoles, GID);
-                bool isBlue = false;
-                bool isRed = false;
-                for (auto r : commandUser.roles) {
-                    if (r == roleIDBlue) {
-                        isBlue = true;
-                    }
-                    if (r == roleIDRed) {
-                        isRed = true;
-                    }
-                }
-
-                if (isBlue == false && isRed == false) {
-                    bot.guild_member_add_role(GID, commandUser.user_id, roleIDRed,
-                        [commandUser, roleIDRed, event](const dpp::confirmation_callback_t& event1) {
-                            if (event1.is_error()) {
-                                std::cout << "Error changing guild member role." << event1.http_info.body << std::endl;
-                            }
-                            else {
-                                std::cout << "Success assigning member: " << commandUser.user_id << " the role: " << roleIDRed << std::endl;
-                                event.edit_response(dpp::message("Success joining the ***Red*** team...").set_flags(dpp::m_ephemeral));
-                            }
-                        });
-                }
-                
-                if (isBlue == true) {
-                    event.edit_response(dpp::message("You are on team ***Blue***! Get out of here you SPY!!! **You cannot change roles after choosing one**").set_flags(dpp::m_ephemeral));
-                }
-
-                if (isRed == true) {
-                    event.edit_response(dpp::message("You are already on the ***Red*** team.").set_flags(dpp::m_ephemeral));
-                }
+            if (cmd_data.name == "invitemessage") {
+                RVB_m::openInvite(bot, GID);
+                event.reply(dpp::ir_channel_message_with_source, dpp::message("Posting the Faction selection message").set_flags(dpp::m_ephemeral));
             }
-        });
+        };
+    });
 
-        bot.on_interaction_create([&](const dpp::interaction_create_t event) {
-            if (event.command.type == dpp::it_application_command) {
-                auto cmd_data = std::get<dpp::command_interaction>(event.command.data);
-                auto GID = event.command.guild_id;
+    bot.on_button_click([&bot](const dpp::button_click_t& event) {
 
-                if (cmd_data.name == "init") {
-                    RVB_i::createCommands(bot, GID);                                        //create the slash commands necessary for the RVB BOT to run.
-                    RVB_i::firstMessages(bot, GID);                                         //generate the first messages the RVB BOT sends to the Guild.
-                    RVB_m::updateGuildRoles(bot, GID, connectedGuildRoles);                 //check/create/store the roles for the RVB BOT
-                }
+        if (event.custom_id == "hero") {
+            event.reply(dpp::ir_channel_message_with_source, dpp::message("Attempting to become a Hero...").set_flags(dpp::m_ephemeral));
+            RVB_m::roleClick(bot, event);
+        }
 
-                if (cmd_data.name == "prm") {
-                    for (auto it : connectedGuildRoles) {
-                        std::cout << "GID: " << it.first;
-                        for (auto it2 : it.second)
-                        {
-                            std::cout << "Role Name: " << it2.second.name << std::endl;
-                        }
-                    }
-                }
+        if (event.custom_id == "villain") {
+            event.reply(dpp::ir_channel_message_with_source, dpp::message("Attempting to become a Villain...").set_flags(dpp::m_ephemeral));
+            RVB_m::roleClick(bot, event);
+        }
 
-                if (cmd_data.name == "getuser") {
-                    auto intArgs = cmd_data.options;
-                    auto targetMemberName = std::get<std::string>(intArgs[0].value);
+        if (event.custom_id == "civilian") {
+            event.reply(dpp::ir_channel_message_with_source, dpp::message("Attempting to become a Civilian...").set_flags(dpp::m_ephemeral));
+            RVB_m::roleClick(bot, event);
+        }
+    });
 
-                    if (auto ptr = dpp::find_guild(GID)) {
-                        dpp::guild g = *ptr;
-                        auto members = g.members;
-                        auto mstring = members;
-                        auto membercount = g.member_count;
+    bot.on_guild_role_create([&bot](const dpp::guild_role_create_t& event) {
+        auto guild = *event.creating_guild;
+        auto GID = guild.id;
+        
+        RVB_m::roleCheck(bot, GID);
+    });
 
-                        std::cout << "Total amount of members in Guild: " << membercount << std::endl;
+    bot.on_channel_create([&bot](const dpp::channel_create_t& event) {
+        auto guild = *event.creating_guild;
+        auto GID = guild.id;
+        auto channel = *event.created;
 
-                        for (auto it : members)
-                        {
-                            if (it.first == 0) {
-                                //std::cout << "Found an invalid member in list..." << std::endl;
-                            }
-                            else {
-                                auto userID = it.first;
-                                auto member = dpp::find_guild_member(GID, userID);
-                                auto jmember = member.build_json();
-                                auto memberRoles = member.roles;
+        if (channel.flags == dpp::c_category) {
+            RVB_m::categoryCreate(bot, GID);
+        }
+    });
 
-                                std::cout << member.user_id << std::endl;
 
-                                if (member.user_id == 313218687874367488) {
+    /* Register Global slash command in on_ready */
+    bot.on_ready([&bot](const dpp::ready_t& event) {
+        /* Wrap command registration in run_once to make sure it doesnt run on every full reconnection */
+        if (dpp::run_once<struct register_bot_commands>()) {
+        
+        }
+        
+        bot.log(dpp::ll_info, "Logged in as " + bot.me.username);
+        /*set initial environment of Global Slash commands that enable the use of the BOT*/
+        RVB_i::startupVerification(bot);
+        
+    });
 
-                                    nlohmann::json jroles(member.build_json());
-                                    std::ofstream jmem("memberJSON.txt");
-                                    jmem << jroles;
 
-                                }
+    /* Output simple log messages to stdout */
+    bot.on_log(dpp::utility::cout_logger());
 
-                                bot.user_get(userID,
-                                    [targetMemberName, member](const dpp::confirmation_callback_t event) {
-                                        if (event.is_error()) {
-                                            std::cout << "Error: user does not exist." << std::endl;
-                                        }
-                                        else {
-                                            auto user = std::get<dpp::user_identified>(event.value);
-
-                                            bool userMatch = false; 
-                                            if (targetMemberName == user.username) {
-                                                userMatch = true;
-                                            }
-                                            else if (targetMemberName == member.nickname) {
-                                                userMatch = true;
-                                            }
-                                            else {
-                                                userMatch = false;
-                                            }
-
-                                            if (userMatch) {
-                                                std::cout << "Found user: " << user.username << std::endl;
-                                                std::string x = (member.nickname != "") ? member.nickname : user.username;
-                                                std::cout << "Guild member name: " << x << std::endl;
-                                                std::cout << "Member Roles: ";
-
-                                                for (auto it : member.roles) {
-                                                    dpp::role tempRole = *dpp::find_role(it);
-                                                    std::cout << tempRole.name << ": " << it << " | ";
-                                                }
-                                                std::cout << std::endl;
-                                            }
-                                        }
-                                    });
-                            }
-                        }
-                    }
-                 };
-            }
-
-         });
-
-        bot.on_message_create([&bot](const dpp::message_create_t& event) {
-            if (event.msg.content == "!ping") {
-                bot.message_create(dpp::message(event.msg.channel_id, "Pong!"));
-            }
-            });
-
-        bot.on_log([](const dpp::log_t& event) {
-            if (event.severity > dpp::ll_trace) {
-                std::cout << dpp::utility::loglevel(event.severity) << ": " << event.message << "\n";
-            }
-            });
-
-        bot.start(false);
-    }
-    catch (const std::exception& e) {
-        std::cout << "Error: " << e.what() << "\n";
-    }
+    /* Start the bot */
+    bot.start(false);
 
     return 0;
 }
